d831d1922cfe2adcc33039baaea29f233ba68740
Johnny Kim <youngbeanies@gmail.com>
2020-11-14 21:57:49 -0800

    here you go john

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MainStats from './components/MainStats';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>BC COVID-19 app by Johnny Kim</Text>
+      <MainStats/>
       <StatusBar style="auto" />
     </View>
   );
