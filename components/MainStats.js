c34c165c04e2bdd200164af1ce72f2b7b7bdd109
John <johnlee46@gmail.com>
2020-11-27 17:59:52 -0800

    update mainstats
    update mainstats with state changing

--- components/MainStats.js
+++ components/MainStats.js
@@ -4,9 +4,11 @@ import { StyleSheet, Text, View } from "react-native";
 import axios from "axios";
 
 class MainStats extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
+            savedData: {},
             reportedDate: 1,
             ha: 2,
             sex: 3,
@@ -15,23 +17,45 @@ class MainStats extends Component {
         };
     }
 
-    loadData() { }
-
-    componentDidMount() {
-        console.log("hey guys");
+    loadData() { 
+        console.log("loadData()");
         axios
             .get("http://localhost:5000/data/", { withCredentials: true })
             .then((response) => {
-                console.log(response.data);
-                this.setState({
-                    reportedDate: response.data[0].Reported_Date,
-                    ha: response.data[0].HA,
-                    sex: response.data[0].Sex,
-                    ageGroup: response.data[0].Age_Group,
-                    classification: response.data[0].Classification_Reported
-                });
-            });
+                //console.log(response.data);
+                this.setState({savedData : response.data});
+                console.log("1 state  saved data")
+                console.log(this.state.savedData);
+                console.log("2 state  saved data")
+            }).then(this.searchInData());
     }
+
+    componentDidMount() {
+      this.loadData();
+        /*console.log("hey guys");
+        axios
+            .get("http://localhost:5000/data/", { withCredentials: true })
+            .then((response) => {
+                //console.log(response.data);
+                this.setState({savedData : response.data}).catch((error)=>{
+                  console.log(error);
+               });;
+            }).then(this.searchInData());*/
+        
+    }
+
+    searchInData(){
+      console.log("3 searchInData()");
+      console.log(this.state.savedData)
+      /*this.setState({
+        reportedDate : savedData[0].Reported_Date,
+        ha: savedData[0].HA,
+        sex: savedData[0].Sex,
+        ageGroup: savedData[0].Age_Group,
+        classification: savedData[0].Classification_Reported
+      })*/
+    }
+
 
     render() {
         return (
