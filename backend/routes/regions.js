7ee77175410dc1caeb4bb2de2df3ab006c142358
youngbeanie <kyb3275@gmail.com>
2020-12-07 14:01:18 -0800

    regions router

---
+++ backend/routes/regions.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+let Data = require("../models/data.model");
+
+router.route("/").get(async (req, res) => {
+    var data = await Data.Data.aggregate([
+      {$group : { _id : '$HA', count : {$sum : 1}}},{$sort: {_id: 1}}]
+   )
+      .then(async (data) => {
+        console.log(data);
+        res.json(data);
+      })
+      .catch((err) => res.status(400).json("Error: " + err));
+  });
+  
+  module.exports = router;
