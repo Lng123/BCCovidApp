9cbc7bb2465e3e2d5c42885f815a97e584a1eabc
John <johnlee46@gmail.com>
2020-12-20 17:00:42 -0800

    updated to skip to row 33899
    updated to skip to row 33899

--- backend/routes/data2.js
+++ backend/routes/data2.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 let Data = require("../models/data.model");
 
 router.route("/").get(async (req, res) => {
-  var data = await Data.Data.find().limit(30000)
+  var data = await Data.Data.find().limit(33899)
     .then(async (data) => {
       console.log(data);
       res.json(data);
